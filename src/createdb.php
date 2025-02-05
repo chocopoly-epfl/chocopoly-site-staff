0666113396787b506fe71902760e2e1448115025
Alam <94713406+TheMathSquare@users.noreply.github.com>
2025-02-05 19:40:05 +0100

    Error reporting

--- src/createdb.php
+++ src/createdb.php
@@ -1,5 +1,9 @@
 <?php
-	
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 	$conn = mysqli_connect($db_host, $db_user, $db_pass);
 	
 	mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS chocopoly");
